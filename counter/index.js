ad7aba74e41f47056581b0e53f10f5c16974082c
Jason Cheung <jasoncheung94@hotmail.com>
2021-10-14 21:19:19 +0100

    Add counter code and auto format document files

--- counter/index.js
+++ counter/index.js
@@ -3,24 +3,24 @@ let decrementButton = document.getElementById("decrement");
 let counter = document.getElementById("counter");
 let count = 0;
 
-incrementButton.addEventListener("click", function(){
-    count++;
-    counter.textContent = count;
-    updateStyles(count);
-})
+incrementButton.addEventListener("click", function () {
+  count++;
+  counter.textContent = count;
+  updateStyles(count);
+});
 
-decrementButton.addEventListener("click", ()=>{
-    count--;
-    counter.textContent = count;
-    updateStyles(count)
-})
+decrementButton.addEventListener("click", () => {
+  count--;
+  counter.textContent = count;
+  updateStyles(count);
+});
 
-function updateStyles(count){
-    if (count<0){
-        counter.classList.add("negative");
-    } else if (count > 0) {
-        counter.classList.add("positive");
-    } else {
-        counter.className = '';
-    }
+function updateStyles(count) {
+  if (count < 0) {
+    counter.classList.add("negative");
+  } else if (count > 0) {
+    counter.classList.add("positive");
+  } else {
+    counter.className = "";
+  }
 }
