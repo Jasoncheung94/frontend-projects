d33850fcca58773c3d02722f4f7bc80ccbf45efd
Jason Cheung <jasoncheung94@hotmail.com>
2021-12-26 20:31:54 +0000

    Add a future date

--- countdown-timer-static/index.js
+++ countdown-timer-static/index.js
@@ -1,6 +1,6 @@
 const countdown = () => {
   // Specify the date and time we are counting down to.
-  const countDate = new Date("Jan 1, 2022 00:00:00").getTime();
+  const countDate = new Date("Jan 1, 2035 00:00:00").getTime();
   const now = new Date().getTime();
   const remainingTime = countDate - now;
 
